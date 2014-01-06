8e698cbdb7a4a623a9c2061b46924342586ef145
SDT1 <eyebeam79@naver.com>
2014-01-06 14:26:43 +0900

    3.3.6. 실습을 완료했습니다.

--- PickerView/ViewController.m
+++ PickerView/ViewController.m
@@ -8,16 +8,61 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIPickerViewDelegate, UIPickerViewDataSource>
+{
+    NSArray *data;
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
 @implementation ViewController
 
+// 컴포넌트개수
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 2;
+}
+
+// 컴포넌트별로 항목개수
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    if (component == 0)
+    {
+        return 10;
+    }
+    else
+    {
+        return [data count];
+    }
+}
+
+// 각컴포넌트와로우인데스에 해당하는 문자열 항목반환
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if (component == 0) {
+        return [NSString stringWithFormat:@"Row: %d", row];
+    }
+    else
+    {
+        return [data objectAtIndex:row];
+    }
+}
+
+// 사용자선택시
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    NSString *item = [self pickerView:pickerView titleForRow:row forComponent:component];
+    self.label.text = [NSString stringWithFormat:@"컴포넌트: %d 로우: %d 항목: %@", component, row, item];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    data = [[NSArray alloc] initWithObjects:@"item1", @"item2", @"item3", @"item4", @"item5", nil];
 }
 
 - (void)didReceiveMemoryWarning
